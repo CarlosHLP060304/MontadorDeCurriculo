4e9f6fed220fc603f7731370ba0f9b648ae430b7
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2024-03-12 18:15:04 -0300

    Add files via upload

---
+++ MontadorDeCurriculo/js/main.js
@@ -0,0 +1,14 @@
+const dadosUsuario = document.querySelectorAll(".dados_usuario")
+const preview = document.querySelector("#preview")
+let curriculo = new Curriculo()
+dadosUsuario.forEach(
+    dado => dado.addEventListener("blur",()=>{
+        
+    })
+)
+
+
+
+
+
+
